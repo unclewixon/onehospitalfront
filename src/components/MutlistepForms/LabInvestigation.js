93abdd96efb8a5f39f4cdf27f30f0c25c2c3de9d
Edebo <akandeadebowale0@gmail.com>
2020-04-28 09:06:48 +0100

    parto ui

--- src/components/MutlistepForms/LabInvestigation.js
+++ src/components/MutlistepForms/LabInvestigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { validate } from '../../services/validationSchemas';
 import {
 	renderTextInput,
@@ -6,20 +7,41 @@ import {
 	renderMultiselect,
 } from '../../services/utilities';
 import { Field, reduxForm } from 'redux-form';
-import Multiselect from 'react-widgets/lib/Multiselect';
+import { notifySuccess, notifyError } from '../../services/notify';
+import { getAllLabTests, getAllLabGroups } from '../../actions/settings';
 
-const fetal = [
-	{
-		value: 'daily',
-		label: 'daily',
-	},
-	{ value: 'weekend', label: 'weekend' },
-	{ value: 'monthly', label: 'monthly' },
-];
+const fetal = [{ id: 'Lab', name: 'Lab' }];
 
 class LabInvestigation extends Component {
+	state = {
+		groups: [],
+		tests: [],
+	};
+	componentDidMount() {
+		console.log(this.props.LabGroups, this.props.LabTests);
+		if (this.props.LabGroups.length === 0) {
+			Promise.all([this.props.getAllLabGroups(), this.props.getAllLabTests()])
+				.then(response => {
+					this.filterGroupsTests(this.props.LabGroups, this.props.LabTests);
+				})
+				.catch(e => {
+					notifyError(
+						e.message || 'could not fetch service categories and services'
+					);
+				});
+		} else {
+			this.filterGroupsTests(this.props.LabGroups, this.props.LabTests);
+		}
+	}
+
+	filterGroupsTests = (grps, tsts) => {
+		const groups = grps.map(el => el.name);
+		const tests = tsts.map(el => el.name);
+		this.setState({ groups, tests });
+	};
 	render() {
 		const { handleSubmit, previousPage, error, page } = this.props;
+		const { groups, tests } = this.state;
 		return (
 			<>
 				<h6 className="element-header">Step {page}. Lab Investigation</h6>
@@ -47,10 +69,10 @@ class LabInvestigation extends Component {
 							<div className="col-sm-6">
 								<label>Lab combination</label>
 								<Field
-									name="lab_combos"
+									name="groups"
 									component={renderMultiselect}
 									defaultValue={[]}
-									data={['Guitar', 'Cycling', 'Hiking']}
+									data={groups}
 								/>
 							</div>
 						</div>
@@ -58,16 +80,16 @@ class LabInvestigation extends Component {
 							<div className="col-sm-6">
 								<label>Lab tests to request</label>
 								<Field
-									name="lab_tests_to_request"
+									name="tests"
 									component={renderMultiselect}
 									defaultValue={[]}
-									data={['Guitar', 'Cycling', 'Hiking']}
+									data={tests}
 								/>
 							</div>
 							<div className="col-sm-6">
 								<Field
-									id="preferred_specimen"
-									name="preferred_specimen"
+									id="preferredSpecimen"
+									name="preferredSpecimen"
 									component={renderTextInput}
 									label="Preferred Specimen(s)"
 									placeholder="Preferred Specimen(s)"
@@ -95,10 +117,19 @@ class LabInvestigation extends Component {
 	}
 }
 LabInvestigation = reduxForm({
-	form: 'antennatal', //Form name is same
+	form: 'antennatalAssessment', //Form name is same
 	destroyOnUnmount: false,
 	forceUnregisterOnUnmount: true, // <------ unregister fields on unmount
 	validate,
 })(LabInvestigation);
 
-export default LabInvestigation;
+const mapStateToProps = state => {
+	return {
+		LabTests: state.settings.lab_tests,
+		LabGroups: state.settings.lab_groups,
+	};
+};
+export default connect(mapStateToProps, {
+	getAllLabGroups,
+	getAllLabTests,
+})(LabInvestigation);
--- src/pages/LabourMgt/Partograph.js
+++ src/pages/LabourMgt/Partograph.js
@@ -1,8 +1,319 @@
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
+import {
+	LineChart,
+	Line,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	Legend,
+	Label,
+	Scatter,
+	ResponsiveContainer,
+} from 'recharts';
 
-export class Partograph extends Component {
+const data = [
+	{
+		name: '0',
+
+		pv: 2400,
+		amt: 2400,
+	},
+	{
+		name: '2',
+
+		pv: 1398,
+		amt: 2210,
+	},
+	{
+		name: '4',
+
+		pv: 9800,
+		amt: 2290,
+	},
+	{
+		name: '6',
+
+		pv: 3908,
+		amt: 2000,
+	},
+	{
+		name: '8',
+
+		pv: 4800,
+		amt: 2181,
+	},
+	{
+		name: '10',
+
+		pv: 3800,
+		amt: 2500,
+	},
+	{
+		name: '12',
+
+		pv: 4300,
+		amt: 2100,
+	},
+	{
+		name: '14',
+
+		pv: 4300,
+		amt: 2100,
+	},
+	{
+		name: '16',
+
+		pv: 4300,
+		amt: 2100,
+	},
+];
+
+export class Partograph extends PureComponent {
 	render() {
-		return <div>Parto graph</div>;
+		return (
+			<>
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Fetal Heart Rate</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Fetal heart rate"
+							label={{
+								value: 'Fetal heart rate',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="Fetal heart rate
+						"
+							stroke="#8884d8"
+							activeDot={{ r: 8 }}
+							margin={{ top: 20 }}
+						/>
+					</LineChart>
+				</div>
+
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Cervic dilation</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Cervic dilation"
+							label={{
+								value: 'Cervic dilation',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="systolic"
+							stroke="red"
+							activeDot={{ r: 8 }}
+							name="systolic"
+						/>
+						<Line
+							name="Diastolic"
+							type="monotone"
+							dataKey="Diastolic"
+							stroke="teal"
+						/>
+						<Line
+							name="tristylic"
+							type="monotone"
+							dataKey="tristylic"
+							stroke="blue"
+						/>
+
+						<Scatter name="red" dataKey="red" fill="red" />
+					</LineChart>
+				</div>
+
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Contraction</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Contraction"
+							label={{
+								value: 'Contraction',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="systolic"
+							stroke="#8884d8"
+							activeDot={{ r: 8 }}
+							name="systolic"
+						/>
+						<Line
+							name="Diastolic"
+							type="monotone"
+							dataKey="Diastolic"
+							stroke="#ff7300"
+						/>
+					</LineChart>
+				</div>
+
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Blood Pressure</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Blood Pressure"
+							label={{
+								value: 'Blood Pressure',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="systolic"
+							stroke="#8884d8"
+							activeDot={{ r: 8 }}
+							name="systolic"
+						/>
+						<Line
+							name="Diastolic"
+							type="monotone"
+							dataKey="Diastolic"
+							stroke="#82ca9d"
+						/>
+					</LineChart>
+				</div>
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Pulse</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Pulse"
+							label={{
+								value: 'Pulse',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="Pulse"
+							stroke="#8884d8"
+							activeDot={{ r: 8 }}
+						/>
+					</LineChart>
+				</div>
+
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Temperature</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Temperature"
+							label={{
+								value: 'Temperature',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="Temperature"
+							stroke="#8884d8"
+							activeDot={{ r: 8 }}
+						/>
+					</LineChart>
+				</div>
+
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Respiration Rate</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Respiration rate"
+							label={{
+								value: 'Respiration rate',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="respiration rate"
+							stroke="#8884d8"
+							margin={{ top: 20 }}
+							activeDot={{ r: 8 }}
+						/>
+					</LineChart>
+				</div>
+
+				<div className="col-md-12 mt-3">
+					<h6 className="text-center">Blood Glucose</h6>
+					<LineChart width={900} height={300} data={data}>
+						<CartesianGrid strokeDasharray="3 3" />
+						<XAxis dataKey="name">
+							<Label value="Labour Time" offset={0} position="insideBottom" />
+						</XAxis>
+						<YAxis
+							dataKey="Blood Glucose"
+							label={{
+								value: 'Blood Glucose',
+								angle: -90,
+								position: 'insideLeft',
+							}}
+						/>
+						<Tooltip />
+						<Legend margin={{ bottom: 20 }} />
+						<Line
+							type="monotone"
+							dataKey="Blood glucose"
+							stroke="#8884d8"
+							margin={{ top: 20 }}
+							activeDot={{ r: 8 }}
+						/>
+					</LineChart>
+				</div>
+			</>
+		);
 	}
 }
 
