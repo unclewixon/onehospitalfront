54b567f5839260a089da713bf11622111306465d
Chukwuemeka Ihedoro <caihedoro@gmail.com>
2022-03-23 09:15:49 +0100

    set create lab permissions

--- src/components/Navigation/ClinicalLabMenu.js
+++ src/components/Navigation/ClinicalLabMenu.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { hasCreateLabPermission } from '../../permission-utils/lab';
+
 const ClinicalLabMenu = () => {
+	const staff = useSelector(state => state.user.profile);
+
 	return (
 		<>
 			<li>
@@ -12,14 +17,16 @@ const ClinicalLabMenu = () => {
 					<span>Dashboard</span>
 				</Link>
 			</li>
-			<li>
-				<Link to="/lab/new-request">
-					<div className="icon-w">
-						<div className="os-icon os-icon-plus-circle" />
-					</div>
-					<span>New Lab Request</span>
-				</Link>
-			</li>
+			{hasCreateLabPermission(staff.permissions) && (
+				<li>
+					<Link to="/lab/new-request">
+						<div className="icon-w">
+							<div className="os-icon os-icon-plus-circle" />
+						</div>
+						<span>New Lab Request</span>
+					</Link>
+				</li>
+			)}
 		</>
 	);
 };
--- src/pages/ClinicalLab/Home.js
+++ src/pages/ClinicalLab/Home.js
@@ -1,9 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { lazy, Suspense } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import NoMatch from '../NoMatch';
 import Splash from '../../components/Splash';
+import { hasCreateLabPermission } from '../../permission-utils/lab';
 
 const LabQueue = lazy(() => import('./LabQueue'));
 const LabRequest = lazy(() => import('../../components/Patient/LabRequest'));
@@ -11,6 +13,8 @@ const AllRequest = lazy(() => import('./AllRequest'));
 
 const ClinicalLab = ({ match, location }) => {
 	const page = location.pathname.split('/').pop();
+
+	const staff = useSelector(state => state.user.profile);
 
 	let pageTitle = 'Lab Queue';
 	if (page === 'all-request') {
@@ -30,23 +34,28 @@ const ClinicalLab = ({ match, location }) => {
 									to={`${match.path}`}
 									className={`mx-2 btn btn-primary btn-sm ${
 										page === 'lab' ? 'btn-outline-primary' : ''
-									}`}>
+									}`}
+								>
 									Lab Queue
 								</Link>
 								<Link
 									to={`${match.path}/all-request`}
 									className={`mr-2 btn btn-primary btn-sm ${
 										page === 'all-request' ? 'btn-outline-primary' : ''
-									}`}>
+									}`}
+								>
 									Lab Requests
 								</Link>
-								<Link
-									to={`${match.path}/new-request`}
-									className={`mr-2 btn btn-primary btn-sm ${
-										page === 'new-request' ? 'btn-outline-primary' : ''
-									}`}>
-									New Lab Request
-								</Link>
+								{hasCreateLabPermission(staff.permissions) && (
+									<Link
+										to={`${match.path}/new-request`}
+										className={`mr-2 btn btn-primary btn-sm ${
+											page === 'new-request' ? 'btn-outline-primary' : ''
+										}`}
+									>
+										New Lab Request
+									</Link>
+								)}
 							</div>
 							<h6 className="element-header">{pageTitle}</h6>
 							<div className="element-content">
